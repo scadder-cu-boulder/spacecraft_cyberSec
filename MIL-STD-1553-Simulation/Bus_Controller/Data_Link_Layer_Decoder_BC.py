30931f973f700bc27fb2fa446ff292df1b62b44f
Rishab Kharidhi <rikh4714@colorado.edu>
2020-02-01 16:36:28 -0700

    Updated and resolved errors on Status Word Decoder.

--- MIL-STD-1553-Simulation/Bus_Controller/Data_Link_Layer_Decoder_BC.py
+++ MIL-STD-1553-Simulation/Bus_Controller/Data_Link_Layer_Decoder_BC.py
@@ -17,47 +17,47 @@ class DataLinkLayerDecoderBC:
             # as it does not affect any data that is necessary
             status_word = {}
 
-            rt_address = rt_address + status_word_frame[3]
+            self.rt_address = self.rt_address + status_word_frame[3]
 
-            addr_char = cmd_word_frame[4:8]
-            rt_address = rt_address + str(hex(int(addr_char,2)))[2:]
-            status_word.update('rt_address':rt_address)
+            addr_char = status_word_frame[4:8]
+            self.rt_address = self.rt_address + str(hex(int(addr_char,2)))[2:]
+            status_word['rt_address']= self.rt_address
 
             # Next bit represents message error bit
-            message_error_bit = cmd_word_frame[8]
-            status_word.update('message_error_bit':message_error_bit)
+            self.message_error_bit = status_word_frame[8]
+            status_word['message_error_bit'] = self.message_error_bit
 
             # Next bit represents instrumentation bit
-            instrumentation_bit = cmd_word_frame[9]
-            status_word.update('instrumentation_bit':instrumentation_bit)
+            self.instrumentation_bit = status_word_frame[9]
+            status_word['instrumentation_bit']=self.instrumentation_bit
 
             # Next bit represents service request bit
-            service_request_bit = cmd_word_frame[10]
-            status_word.update('service_request_bit':service_request_bit)
+            self.service_request_bit = status_word_frame[10]
+            status_word['service_request_bit']=self.service_request_bit
 
             # Next 3 bits represent reserved bits
-            reserved_bits = cmd_word_frame[11:14]
-            status_word.update('reserved_bits':reserved_bits)
+            self.reserved_bits = status_word_frame[11:14]
+            status_word['reserved_bits']=self.reserved_bits
 
             # Next bit represents BRDCST received bit
-            brdcst_received_bit = cmd_word_frame[14]
-            status_word.update('brdcst_received_bit':brdcst_received_bit)
+            self.brdcst_received_bit = status_word_frame[14]
+            status_word['brdcst_received_bit']=self.brdcst_received_bit
 
             # Next bit represents busy bit
-            busy_bit = cmd_word_frame[15]
-            status_word.update('busy_bit':busy_bit)
+            self.busy_bit = status_word_frame[15]
+            status_word['busy_bit']=self.busy_bit
 
             # Next bit represents subsystem flag bit
-            subsystem_flag_bit = cmd_word_frame[16]
-            status_word.update('subsystem_flag_bit':subsystem_flag_bit)
+            self.subsystem_flag_bit = status_word_frame[16]
+            status_word['subsystem_flag_bit']=self.subsystem_flag_bit
 
             # Next bit represents dynamic bus control accept bit
-            dynamic_bus_control_accpt_bit = cmd_word_frame[17]
-            status_word.update('dynamic_bus_control_accpt_bit':dynamic_bus_control_accpt_bit)
+            self.dynamic_bus_control_accpt_bit = status_word_frame[17]
+            status_word['dynamic_bus_control_accpt_bit']=self.dynamic_bus_control_accpt_bit
 
             # Next bit represents terminal flag bit
-            terminal_flag_bit = cmd_word_frame[18]
-            status_word.update('terminal_flag_bit':terminal_flag_bit)
+            self.terminal_flag_bit = status_word_frame[18]
+            status_word['terminal_flag_bit']=self.terminal_flag_bit
 
             return status_word
         except Exception as ex:
